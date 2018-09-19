1bb2815970d3b9fb10ef1c3f986590b7cb1f53ce
abc <abc@example.com>
2018-09-19 22:53:35 +0300

    zx

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;
+var ret=false;
+for (var i = 0; i < preferences.length; i++)
+{
+    var f1,f2;
+    f1 = preferences[preferences[i]-1];
+    if (preferences.length+1>f1)
+    {
+        f2 = preferences[f1-1];
+        if (f2 == i+1) count++;
+    }
+}
+ret=Math.trunc(count/3);
+return ret;
 };
